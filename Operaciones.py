f979d60dd152bc06945ddc0a4a3f107c1b0f2b04
Max-APL <maxpasten15908@gmail.com>
2024-03-26 09:31:53 -0400

    Pruebas de Operaciones

--- Operaciones.py
+++ Operaciones.py
@@ -6,10 +6,20 @@ def suma(a, b):
 
 def resta(a, b):
     return a - b    
+
 def Multi(a, b):
     return a*b
+
 def division(a, b):
     try:
         return a / b
     except ZeroDivisionError:
         return "No se puede dividir por cero"
+
+# Pruebas
+if __name__ == "__main__":
+    print(suma(5, 3))
+    print(resta(5, 3))
+    print(Multi(5, 3))
+    print(division(5, 3))
+    print(division(5, 0))

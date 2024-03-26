bd87a1c122b8fc2745f3dac273b1cf367a245110
EsauuR <165038332+EsauuR@users.noreply.github.com>
2024-03-26 09:30:29 -0400

    Update Operaciones.py

--- Operaciones.py
+++ Operaciones.py
@@ -8,4 +8,8 @@ def resta(a, b):
     return a - b    
 def Multi(a, b):
     return a*b
-
+def division(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return "No se puede dividir por cero"
